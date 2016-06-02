97f7f01fae56e60c723065a1ce4422acc644168e
Rafael da Silva Ferreira <rafael@swift-yah.io>
2016-06-02 14:05:03 -0300

    Remove Pods folder from git cached folder.

--- Example/Pods/Target Support Files/PaperTrailLog/PaperTrailLog-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PaperTrailLog : NSObject
-@end
-@implementation PodsDummy_PaperTrailLog
-@end
--- Example/Pods/Target Support Files/PaperTrailLog/PaperTrailLog-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double PaperTrailLogVersionNumber;
-FOUNDATION_EXPORT const unsigned char PaperTrailLogVersionString[];
-
